6f9b5b48eb49c2c5ba11b6c8b81f63e988ead273
Yann Dubois <yann59.dubois@gmail.com>
2017-02-28 20:15:31 +0100

    Button bug correction

--- SimpleButton/src/e201/skeleton/SimpleButton.java
+++ SimpleButton/src/e201/skeleton/SimpleButton.java
@@ -1,22 +1,26 @@
-package e201.skeleton ;
+package e201.skeleton;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Font ;
+import java.awt.Font;
 import java.awt.Paint;
 import java.awt.Stroke;
 
-import javax.swing.JFrame ;
+import javax.swing.JFrame;
 
 import fr.lri.swingstates.canvas.CExtensionalTag;
 import fr.lri.swingstates.canvas.CRectangle;
-import fr.lri.swingstates.canvas.CShape ;
+import fr.lri.swingstates.canvas.CShape;
 import fr.lri.swingstates.canvas.CStateMachine;
 import fr.lri.swingstates.canvas.CText;
-import fr.lri.swingstates.canvas.Canvas ;
+import fr.lri.swingstates.canvas.Canvas;
+import fr.lri.swingstates.canvas.transitions.EnterOnShape;
 import fr.lri.swingstates.canvas.transitions.EnterOnTag;
+import fr.lri.swingstates.canvas.transitions.LeaveOnShape;
 import fr.lri.swingstates.canvas.transitions.LeaveOnTag;
+import fr.lri.swingstates.canvas.transitions.PressOnShape;
 import fr.lri.swingstates.canvas.transitions.PressOnTag;
+import fr.lri.swingstates.canvas.transitions.ReleaseOnShape;
 import fr.lri.swingstates.canvas.transitions.ReleaseOnTag;
 import fr.lri.swingstates.debug.StateMachineVisualization;
 import fr.lri.swingstates.sm.State;
@@ -29,102 +33,100 @@ import fr.lri.swingstates.sm.transitions.Release;
  */
 public class SimpleButton {
 
-    private CText label ;
-    private CRectangle rect;
-    protected CStateMachine stateMachine;
+	private CText label;
+	private CRectangle rect;
+	protected CStateMachine stateMachine;
 
-    SimpleButton(Canvas canvas, String text) {
-	   label = canvas.newText(0, 0, text, new Font("verdana", Font.PLAIN, 12));
-	   rect = canvas.newRectangle(label.getMinX() - 10, label.getMinY() - 10, text.length()*12/2 + 6 + 20, 12 + 20);
-	   rect.setFillPaint(Color.white);
-	   rect.setParent(label);
-	   label.above(rect);
-	   
-	   rect.addTag("rectangle");
-	   
-	   this.stateMachine = new CStateMachine() {
-		   Paint initColor;
-		   Stroke initStroke;
-		   State idle = new State() {
-			public void enter() {
-//				SimpleButton.this.rect.setFillPaint(bla);
-			}
+	SimpleButton(Canvas canvas, String text) {
+		label = canvas.newText(0, 0, text, new Font("verdana", Font.PLAIN, 12));
+		rect = canvas.newRectangle(label.getMinX() - 10, label.getMinY() - 10, text.length() * 12 / 2 + 6 + 20,
+				12 + 20);
+		rect.setFillPaint(Color.white);
+		rect.setParent(label);
+		label.above(rect);
 
-				Transition io = new EnterOnTag("rectangle", ">> over"){
-		    		public void action() {
-		    			initStroke = getShape().getStroke();
-		    			getShape().setStroke(new BasicStroke(3));
-		    		}
-		    	};
+		rect.addTag("rectangle");
+
+		this.stateMachine = new CStateMachine() {
+			Paint initColor;
+			Stroke initStroke;
+			State idle = new State() {
+				public void enter() {
+					SimpleButton.this.rect.setFillPaint(Color.WHITE);
+				}
+
+				Transition io = new EnterOnShape(">> over");
 			};
-			
+
 			State over = new State() {
-				Transition oi = new LeaveOnTag("rectangle", ">> idle") {
-					public void action() {
-						getShape().setStroke(initStroke);
-					}
-				};
+				public void enter() {
+					initStroke = SimpleButton.this.rect.getStroke();
+					SimpleButton.this.rect.setStroke(new BasicStroke(3));
+				}
 				
-				Transition op = new PressOnTag("rectangle", 1, ">> pressed") {
+				Transition oi = new LeaveOnShape(">> idle") {
 					public void action() {
-						initColor = getShape().getFillPaint();
-						getShape().setFillPaint(Color.YELLOW);
-						getShape().setStroke(initStroke);
+						SimpleButton.this.rect.setStroke(initStroke);
 					}
 				};
+
+				Transition op = new PressOnShape(1, ">> pressed");
 			};
-			
+
 			State pressed = new State() {
-				Transition po = new ReleaseOnTag("rectangle", 1, ">> over") {
-					public void action() {
-						getShape().setStroke(new BasicStroke(3));
-						getShape().setFillPaint(initColor);
-					}
-				};
+				public void enter() {
+					initColor = SimpleButton.this.rect.getFillPaint();
+					SimpleButton.this.rect.setFillPaint(Color.YELLOW);
+					SimpleButton.this.rect.setStroke(initStroke);
+				}
 				
-				Transition pd = new LeaveOnTag("rectangle", ">> deactivated") {
+				Transition po = new ReleaseOnShape(1, ">> over") {
 					public void action() {
-						getShape().setFillPaint(initColor);
+						SimpleButton.this.rect.setFillPaint(initColor);
+						SimpleButton.this.action();
 					}
 				};
+
+				Transition pd = new LeaveOnShape(">> deactivated");
 			};
-			
+
 			State deactivated = new State() {
-				Transition dp = new EnterOnTag("rectangle", ">> pressed") {
-					public void action(){
-						getShape().setFillPaint(Color.YELLOW);
-					}
-				};
+				public void enter() {
+					SimpleButton.this.rect.setFillPaint(initColor);
+				}
 				
-				Transition di = new Release(1, ">> idle") {};
+				Transition dp = new EnterOnShape(">> pressed");
+
+				Transition di = new Release(1, ">> idle") {
+				};
 			};
-			
+
 		};
 		this.stateMachine.attachTo(canvas);
-    }
+	}
 
-    public void action() {
-	   System.out.println("ACTION!") ;
-    }
+	public void action() {
+		System.out.println("ACTION!");
+	}
 
-    public CShape getShape() {
-	   return label ;
-    }
+	public CShape getShape() {
+		return label;
+	}
 
-    static public void main(String[] args) {    	
-	   JFrame frame = new JFrame() ;
-	   Canvas canvas = new Canvas(400,400) ;
-	   frame.getContentPane().add(canvas) ;
-	   frame.pack() ;
-	   frame.setVisible(true) ;
+	static public void main(String[] args) {
+		JFrame frame = new JFrame();
+		Canvas canvas = new Canvas(400, 400);
+		frame.getContentPane().add(canvas);
+		frame.pack();
+		frame.setVisible(true);
 
-	   SimpleButton simple = new SimpleButton(canvas, "simple") ;
-	   simple.getShape().translateBy(100,100);
-	   
-	   	JFrame viz = new JFrame() ;
-   		viz.getContentPane().add(new StateMachineVisualization(simple.stateMachine)) ;
-   		viz.pack() ;
-   		viz.setVisible(true) ;
-    }
+		SimpleButton simple = new SimpleButton(canvas, "simple");
+		simple.getShape().translateBy(100, 100);
+
+		JFrame viz = new JFrame();
+		viz.getContentPane().add(new StateMachineVisualization(simple.stateMachine));
+		viz.pack();
+		viz.setVisible(true);
+	}
 
 }
